1d9219f7c15d71af55251e3d042bced5727aa48d
kej9026 <kej9026@naver.com>
2025-11-30 15:32:37 +0900

    프론트엔드 주소 추가

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 import database
 from models import models
 from routers import food_router, history_router, recommendation_router, user_router, admin_router
+from fastapi.middleware.cors import CORSMiddleware
 
 # 테이블 생성
 @asynccontextmanager
@@ -12,6 +13,20 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(title="EcoNutri API", lifespan=lifespan, openapi_version="3.0.2")
+
+origins = [
+    "http://localhost:3000",             # 프론트엔드 로컬 개발 주소
+    "http://127.0.0.1:3000",             
+    "https://dongjae-isaac.github.io",   
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,      # 허용할 사이트 목록
+    allow_credentials=True,     # 로그인 쿠키 등 허용
+    allow_methods=["*"],        # GET, POST, PUT, DELETE 다 허용
+    allow_headers=["*"],        # 모든 헤더 허용
+)
 
 # 라우터 등록
 app.include_router(food_router.router)
